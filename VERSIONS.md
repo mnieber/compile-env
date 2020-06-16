b4069f68f8250619da0dcdd35a85392e0fa22ec1
Maarten Nieber <hallomaarten@yahoo.com>
2020-06-16 13:07:48 +0200

    Bump version to 0.1.2

--- VERSIONS.md
+++ VERSIONS.md
@@ -1,5 +1,5 @@
 # Version history
 
-## 0.1.1
+## 0.1.2
 
 - Add compile-env
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(name='docker_compose_env',
-      version='0.1.1',
+      version='0.1.2',
       description=
       'Solve the problem that environment variables are not interpolated by docker-compose',
       url='https://github.com/mnieber/docker_compose_env',
-      download_url='https://github.com/mnieber/docker_compose_env/tarball/0.1.1',
+      download_url='https://github.com/mnieber/docker_compose_env/tarball/0.1.2',
       author='Maarten Nieber',
       author_email='hallomaarten@yahoo.com',
       license='MIT',
