ad84fe989694ada67f90f487814bf8fd0e10d6aa
Maarten Nieber <hallomaarten@yahoo.com>
2022-06-29 08:49:58 +0200

    Bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="compile-env",
-    version="0.4.0",
+    version="0.4.1",
     description="Solve the problem that environment variables are not interpolated by docker-compose",
     url="https://github.com/mnieber/compile-env",
-    download_url="https://github.com/mnieber/compile-env/tarball/0.3.0",
+    download_url="https://github.com/mnieber/compile-env/tarball/0.4.1",
     author="Maarten Nieber",
     author_email="hallomaarten@yahoo.com",
     license="MIT",
